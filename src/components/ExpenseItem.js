5cdea97628a3b8ea1f86453c0ed16fe725e5862c
Pervez Quadri <pervez8ktt@gmail.com>
2021-09-29 22:47:46 +0530

    init

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
+    return (
+        <div className="expense-item">
+            <div>{props.expenseDate.toISOString()}</div>
+            <div className="expense-item__description"><h2>{props.expenseTitle}</h2></div>
+            <div className="expense-item__price">${props.expenseAmount}</div>
+        </div>
+    );
+
+}
+
+export default ExpenseItem;
